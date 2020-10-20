ebd318f5301cf4ea4158749f70961b4764120c07
Victor Veytsman <vityavv@gmail.com>
2020-10-20 17:51:13 -0400

    October 20th Homework + .gitignore

--- Demo.java
+++ Demo.java
@@ -10,5 +10,46 @@ public class Demo {
 			}
 			System.out.println();
 		}
+		System.out.println(arrDeepToString(create2DArray(5, 5, 5)));
+		System.out.println(arrDeepToString(create2DArrayRandomized(5, 5, 5)));
+	}
+	public static int[][] create2DArray(int rows, int cols, int maxValue) {
+		int[][] result = new int[rows][cols];
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				result[i][j] = (int)(Math.random() * maxValue);
+			}
+		}
+		return result;
+	}
+	public static int[][] create2DArrayRandomized(int rows, int cols, int maxValue) {
+		int[][] result = new int[rows][];
+		for (int i = 0; i < rows; i++) {
+			result[i] = new int[(int)(Math.random() * cols)];
+			for (int j = 0; j < result[i].length; j++) {
+				result[i][j] = (int)(Math.random() * maxValue);
+			}
+		}
+		return result;
+	}
+	public static String arrDeepToString(int[][] arr) {
+		String res = "{";
+		for (int i = 0; i < arr.length; i++) {
+			res += arrToString(arr[i]);
+			if (i < arr.length - 1) {
+				res += ", ";
+			}
+		}
+		return res + "}";
+	}
+	public static String arrToString(int[] arr){
+		String res = "{";
+		for (int i = 0; i < arr.length; i++) {
+			res += String.valueOf(arr[i]);
+			if (i < arr.length - 1) {
+				res += ", ";
+			}
+		}
+		return res + "}";
 	}
 }
